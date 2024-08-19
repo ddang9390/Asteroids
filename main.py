2756efc444b7aaa37720523d32576c415de3d934
ddang9390 <dnld1ng@yahoo.com>
2024-08-19 13:24:28 -0400

    Add simple screen for game

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip() # For refreshing screen
+
+        # For checking if user closed window and exit game loop if they do (make close button work)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
 if __name__ == "__main__":
     main()
